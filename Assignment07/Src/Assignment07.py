3b36128d0851b7599da6192ceb7dc329c57bd991
hogan <hogan@DESKTOP-CJM6TKP.wifi.uc.edu>
2024-02-29 12:37:17 -0500

    First Commit

--- Assignment07/Src/Assignment07.py
+++ Assignment07/Src/Assignment07.py
@@ -1,3 +1,13 @@
+# Name: Cameron Hogan
+# email: hogancg@mail.uc.edu
+# Assignment Number: Assignment 07
+# Due Date: 2/29/2024
+# Course/Section: IS 4010
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: Adding parameters to customize captcha generator.
+# Brief Description of what this module does: Contains functions for generating random strings of numbers and letters, drawing random shapes, and saving the captcha image to a file.
+# Citations:
+# Anything else that's relevant:
 '''
 Created on Feb 26, 2020
 
@@ -11,13 +21,27 @@ default_color_green = 150
 default_color_blue = 150
 
 def generate_random_string(length):
+    """
+    Generate a random string of numbers and letters.
+    
+    Args:
+        length (int): Length of the random string.
+    
+    Returns:
+        str: Random string of numbers and letters.
+    """
     random_string = ""
     for i in range(0,length):
         random_string = random_string + random.choice('1234567890ABCDEFGHIJKLMNOPQRSTUVQXYZ')
     return random_string
 
 def draw_random_ellipse(draw):
-    # A random circle on the image
+    """
+    Draw a random ellipse on the image.
+    
+    Args:
+        draw (PIL.ImageDraw.Draw): Drawing context.
+    """
     a = random.randrange(10, 300, 1)
     b = random.randrange(10, 275, 1)
     c = a + random.randrange(10, 90, 1)
@@ -27,27 +51,35 @@ def draw_random_ellipse(draw):
                                   default_color_blue + random.randrange(-100,100,1), 255), 
                                   outline = "black")
 
-def generate_captcha():
-    '''
-    Generate a captcha
-    :return: A tuple (image, captcha string encoded in the image)
-    '''
-    captcha_string = generate_random_string(5)
-#   print(">" + captcha_string + "<")
+def generate_captcha(num_chars, filename):
+    """
+    Generate a captcha image with a random string of numbers and letters.
+    
+    Args:
+        num_chars (int): Number of characters in the captcha string.
+        filename (str): Filename where the captcha should be stored.
+    
+    Returns:
+        tuple: A tuple containing the captcha image and the captcha string.
+    """
+    if not 6 <= num_chars <= 10:
+        raise ValueError("Number of characters should be between 6 and 10.")
+
+    captcha_string = generate_random_string(num_chars)
     captcha_image = Image.new("RGBA", (400, 200), (default_color_red,default_color_green,default_color_blue))
     draw = ImageDraw.Draw(captcha_image, "RGBA")
     for i in range(1,20):
         draw_random_ellipse(draw)
 
-    fontStyle = ImageFont.truetype("Aaargh.ttf", 48)     # font must be in the same folder as the .py file. 
+    fontStyle = ImageFont.truetype("Aaargh.ttf", 48)
 
-    # Arbitrary starting co-ordinates for the text we will write
     x = 10 + random.randrange(0, 100, 1)
     y = 79 + random.randrange(-10, 10, 1)
     for letter in captcha_string:
-#       print(letter)
-        draw.text((x, y), letter, (0,0,0),font=fontStyle)    # Write in black
+        draw.text((x, y), letter, (0,0,0),font=fontStyle)
         x = x + 35
         y = y +  random.randrange(-10, 10, 1)
     
-    return (captcha_image, captcha_string)  # return a heterogeneous tuple
+    captcha_image.save(filename)
+
+    return (captcha_image, captcha_string)
--- Assignment07/Src/Main.py
+++ Assignment07/Src/Main.py
@@ -1,3 +1,13 @@
+# Name: Cameron Hogan
+# email: hogancg@mail.uc.edu
+# Assignment Number: Assignment 07
+# Due Date: 2/29/2024
+# Course/Section: IS 4010
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: Adding parameters to customize captcha generator.
+# Brief Description of what this module does: Contains functions for generating random strings of numbers and letters, drawing random shapes, and saving the captcha image to a file.
+# Citations:
+# Anything else that's relevant:
 '''
 Created on Feb 26, 2020
 
@@ -6,9 +16,15 @@ Created on Feb 26, 2020
 
 from Src.Assignment07 import generate_captcha
 
-result = generate_captcha()
-myCaptcha = result[0]
-captcha_string = result[1]
-print(">" + captcha_string + "<")
-myCaptcha.show()
+def main():
+    num_chars = 6
+    filename = "captcha_image.png"
 
+    captcha_image, captcha_string = generate_captcha(num_chars, filename)
+
+    print("Captcha String: >", captcha_string, "<")
+
+    captcha_image.show()
+
+if __name__ == "__main__":
+    main()
